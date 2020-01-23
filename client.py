df81ae9067503cae62d09944d102a0921bea9fe7
Max <syvokobylenko@protonmail.com>
2020-01-23 12:16:51 +0100

    updated client to be object based

--- client.py
+++ client.py
@@ -1,10 +1,34 @@
 import socket, time, pyautogui
 
-posold = [0,0]
+class connection:
+    def __init__(self, IP, port, lenght):
+        self.IP = IP
+        self.port = port
+        self.lenght = lenght
+        self.startConnection()
 
-s = socket.socket()
-s.connect(('127.0.0.1', 12345))
+    def startConnection(self):
+        self.s = socket.socket()
+        try:
+            self.s.connect((self.IP, self.port))
+        except(ConnectionRefusedError):
+            time.sleep(5)
+            self.startConnection()
+        except(KeyboardInterrupt):
+            exit
+        
+    def recieveData(self):
+        try:
+            return self.s.recv(self.lenght).decode()
+        except(ConnectionResetError):
+            self.s.close
+            self.startConnection()
+            return self.recieveData()
+        except(KeyboardInterrupt):
+            exit
+            
+data = connection("127.0.0.1", 12345, 9)
 
 while True:
-    pos = s.recv(9).decode().split("#")
+    pos = data.recieveData().split("#")
     pyautogui.moveTo(int(pos[0]), int(pos[1]), 0)
