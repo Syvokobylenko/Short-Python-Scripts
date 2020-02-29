3f8b353f5f081517da984544d76e08bc23722edb
Morosov <morosov@pop-os.localdomain>
2020-02-29 18:42:50 +0100

    changed writefile to be able to handle to console as well as to file writing

--- writeafile.py
+++ writeafile.py
@@ -2,11 +2,13 @@ import time, pyautogui
 from pyautogui import write as writepy
 from pyautogui import press as presspy
 time.sleep(5)
-#pyautogui.write("f = open('boot.py', 'w')")
+pyautogui.write("f = open('boot.py', 'w')")
 pyautogui.press('enter')
 file1 = open('copy.txt', 'r')
 f = file1.readlines()
 for line in f:
- #pyautogui.write("f.write('" + line.rstrip('\n') + "\\n')")
- pyautogui.write(line.rstrip('\n'))
+ pyautogui.write("f.write('" + line.rstrip('\n') + "\\n')")
+ #for i in range(1,10):
+ # pyautogui.press('backspace')
+ #pyautogui.write(line.rstrip('\n'))
  pyautogui.press('enter')
