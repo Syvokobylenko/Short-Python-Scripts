c6f04e19c9d0edb16e52915ad992ee9fbef13546
syvokobylenko <syvokobylenko@protonmail.com>
2020-03-19 15:47:24 +0100

    changed writeafile to use only single line

--- writeafile.py
+++ writeafile.py
@@ -1,6 +1,4 @@
 import time, pyautogui
-from pyautogui import write as writepy
-from pyautogui import press as presspy
 time.sleep(5)
 pyautogui.write("import os")
 pyautogui.press('enter')
@@ -14,12 +12,11 @@ pyautogui.write("f = open('boot.py', 'w')")
 pyautogui.press('enter')
 file1 = open('copy.txt', 'r')
 f = file1.readlines()
+write_string = "f.write(r'"
 for line in f:
- pyautogui.write("f.write(r'" + line.rstrip('\n') + r"' + '\n')")
- #for i in range(1,10):
- # pyautogui.press('backspace')
- #pyautogui.write(line.rstrip('\n'))
- pyautogui.press('enter')
+ write_string += repr(line)
+pyautogui.write("f.write(r'" + write_string + "')")
+pyautogui.press('enter')
 pyautogui.write("f.close()")
 pyautogui.press('enter')
 
