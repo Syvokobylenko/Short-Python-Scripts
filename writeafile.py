07f179428350b6b31d400544c34554e5154cdeaf
syvokobylenko <syvokobylenko@protonmail.com>
2020-05-17 20:58:30 +0200

    changed writeafile to be faster

--- writeafile.py
+++ writeafile.py
@@ -1,22 +1,19 @@
 import time, pyautogui
 time.sleep(5)
+filename = "wifi.ini"
 pyautogui.write("import os")
 pyautogui.press('enter')
-pyautogui.write("os.remove('boot.py')")
+pyautogui.write("os.remove('" + filename + "')")
 pyautogui.press('enter')
-pyautogui.write("f = open('boot.py', 'x')")
-pyautogui.press('enter')
-pyautogui.write("f.close()")
-pyautogui.press('enter')
-pyautogui.write("f = open('boot.py', 'w')")
-pyautogui.press('enter')
-file1 = open('copy.txt', 'r')
-f = file1.readlines()
-write_string = "f.write(r'"
-for line in f:
- write_string += repr(line)
-pyautogui.write("f.write(r'" + write_string + "')")
+pyautogui.write("f = open('" + filename + "', 'x')")
 pyautogui.press('enter')
 pyautogui.write("f.close()")
 pyautogui.press('enter')
-
+pyautogui.write("f = open('" + filename + "', 'w')")
+pyautogui.press('enter')
+file1 = open(filename, 'r')
+f = file1.read()
+pyautogui.write("f.write(" + repr(f) + ")")
+pyautogui.press('enter')
+pyautogui.write("f.close()")
+pyautogui.press('enter')
